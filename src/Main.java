c4b6e81058f561ae93b8d3eb7f5c88c2b9c72763
meirinder <s.kulizhnikov@gmail.com>
2018-02-14 23:59:01 +0700

    demoVersion app. Added simple graphic interface

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,12 @@
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
-import java.io.BufferedReader;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Objects;
-import java.util.Scanner;
 //"https://www.cbr-xml-daily.ru/daily_json.js"
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        Scanner in = new Scanner(System.in);
-        System.out.print("Начальная валюта: ");
-        String fromValute = in.nextLine();
-        System.out.print("Конечная валюта: ");
-        String toValute = in.nextLine();
-        System.out.print("Количество: ");
-        double count = in.nextDouble();
-        Converter converter = new Converter(fromValute,toValute,count);
 
-        System.out.println(converter.getResult());
+        SimpleGUI app = new SimpleGUI();
+        app.setVisible(true);
 
     }
 }
